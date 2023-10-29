31e5ba5a2e22f14ced0e39eebbb1362783c77a3d
Powermacintosh <58847297+Powermacintosh@users.noreply.github.com>
2023-10-29 10:26:57 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,20 +7,17 @@ tg.expand();
 tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#FF00FF';
 
-// получаем кнопку из index.html
-let btn = document.getElementById('btn');
-
-btn.addEventListener('click', function(){
-	tg.MainButton.setText('Сообщение прочитано!');
-	tg.MainButton.show();
-	tg.sendData('sendTestMessage');
-});
-
 window.addEventListener('load', function() {
-	let btn = document.querySelector('.btn');
+	let btn = document.getElementById('btn');
 	document.addEventListener('change', fCheck);
 
 	function fCheck() {
 		btn.style.display = document.querySelectorAll('.checkbox:checked').length ? 'inline' : 'none';
 	}
+});
+
+btn.addEventListener('click', function(){
+	tg.MainButton.setText('Сообщение прочитано!');
+	tg.MainButton.show();
+	tg.sendData('sendTestMessage');
 });
